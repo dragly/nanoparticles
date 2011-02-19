8153d8319a60d6fc156470bffb583bc6f564350f
Svenn-Arne Dragly <s@dragly.com>
2011-02-19 21:32:18 +0100

    Lowered damping. Higher spring constant.

--- particle.cpp
+++ particle.cpp
@@ -10,9 +10,9 @@
 #include <math.h>
 
 const qreal dechargeRate = 0.2;
-const qreal springConstant = 45.0;
+const qreal springConstant = 75.0;
 const qreal minimumCharge = 2.5;
-const qreal dampingFactor = 0.2; // a scaling of the damping force
+const qreal dampingFactor = 0.12; // a scaling of the damping force
 const qreal particleMass = 1.0;
 
 Particle::Particle() :
