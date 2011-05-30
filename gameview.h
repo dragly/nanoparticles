c00f147764d636c96f18f54cb7a6a73fe092e672
Svenn-Arne Dragly <s@dragly.com>
2011-05-30 12:45:57 +0200

    Pausing game when screen is locked on Maemo.

--- gameview.h
+++ gameview.h
@@ -2,6 +2,9 @@
 #define GAMEVIEW_H
 
 #include <QGraphicsView>
+#ifdef Q_WS_MAEMO_5
+#include <QtDBus>
+#endif
 #include "gamescene.h"
 
 class GameView : public QGraphicsView
@@ -13,13 +16,21 @@ public:
 signals:
 
 public slots:
+#ifdef Q_WS_MAEMO_5
+    void displayStateChanged(const QDBusMessage &message);
+    void setDisplayState(const QString &state);
+    void displayStateError(const QDBusError &error);
+#endif
 
 protected:
     void resizeEvent(QResizeEvent *event);
     void changeEvent(QEvent *event);
+
 private:
     GameScene gameScene;
-
+#ifdef Q_WS_MAEMO_5
+    QDBusInterface* dBusInterface;
+#endif
 };
 
 #endif // GAMEVIEW_H
